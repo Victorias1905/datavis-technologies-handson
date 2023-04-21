7e73b5b94d4ebc16918e812cd9378dc170af82c9
Victorias1905 <victoria.strahova@student.uhasselt.be>
2023-04-21 18:55:19 +0300

    exercise_8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,9 @@
+<script>
+let student = { name: 'Victoria', property1: 'Hiking',property2: 'Travelling',property3: 'Driving'};
+</script>
+ <p> My name is {student.name}, and these are some of my favourite things: </p>
+ <div>
+<p>{student.property1}</p>
+<p>{student.property2}</p>
+<p>{student.property3}</p>
+</div>

d5b5fa00cd069243bb37f35598737acc8c5720ca
Victorias1905 <victoria.strahova@student.uhasselt.be>
2023-04-21 19:13:36 +0300

    exercise_9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,9 @@
+<script>
+let r=5;
+function calculation(r) {
+    const circumference = 2*r*Math.PI;
+    return (circumference);
+}
+
+</script>
+The circumference of a circle with radius {r}  is {calculation(r).toFixed(2)}

4d33c3c6121cba06d8a2bf80eb7b4f1753dc11c2
Victorias1905 <victoria.strahova@student.uhasselt.be>
2023-05-01 21:38:57 +0300

    #14

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -3,14 +3,26 @@
 </script>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class:selected={current === 'messi'}
+class="btn btn-secondary" on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button class:selected={current === 'ronaldo'}
+class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class:selected={current === 'neymar'}
+class="btn btn-secondary" on:click={() => (current = "neymar")}>
   Neymar
 </button>
+
+<style>
+  .selected {
+    background-color: lightpink;
+  }
+</style>
+
+
+
